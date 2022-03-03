e3eaa8573659040ecfd238d50a5288dcf09a2139
chaiya1239900 <52321420+chaiya1239900@users.noreply.github.com>
2022-03-03 18:02:58 +0700

    Add files via upload

---
+++ c2-php8-main/index.php
@@ -0,0 +1,51 @@
+<!-- 	Header Start -->
+<?php include("include/header.php"); ?>
+<!-- Header End -->
+
+<!-- NavBar Start -->
+<?php include("include/navbar.php") ?>
+<!-- NavBar end -->
+
+<!-- slider Start -->
+<?php include("include/silder.php") ?>
+<!-- slider End -->
+
+<!-- footer Start -->
+<?php include("include/footer.php")  ?>
+<!-- footer End -->
+
+<!-- Script Start -->
+<?php include("include/script.php") ?>
+<!-- Script End -->
+
+<!-- About as Start -->
+<section class="section">
+	<div class="container-fluid mt-3">
+		<div class="row">
+
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-body shadow">
+						<div class="card-text">
+							วิทยาลัยเทคนิคตราด
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-body shadow">
+						<div class="card-text">
+							ประวัติส่วนตัว
+						</div>
+						<ul type="">
+							<li>สำเร็จการศึกษา ประถมศึกษาขั้นปีที่ 6 ณ โรงเรียน วัดท่าโสม ปี พ.ศ 2559</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!-- About as End -->
